5cc731a55757eb29d62a262c904a8cb1bd1eac4b
nd10 <nd10@narangdesign.com>
2021-03-08 17:48:05 +0900

    Initial commit

---
+++ src/test/java/com/example/ks3/Ks3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ks3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ks3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
